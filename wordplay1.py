fa92c5d00157a222c8f09a416fe6edf44d0c130c
Vijaisriraam <VijaiGopalS@dal.ca>
2019-01-30 23:12:45 -0400

    this is first commit for this file

---
+++ wordplay1.py
@@ -0,0 +1,10 @@
+fin=open('words.txt')
+def word_search(name):
+	count=0
+	for line in fin:
+		word=line.strip()
+		if('e'in word):
+			print(word)
+			count=count+1
+	print('count is:',count)
+word_search('words.txt')
